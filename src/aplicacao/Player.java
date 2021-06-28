8e9c35fe4edaf1cd44cae6cb36a90b03b2764d1a
gustavo <guguines@hotmail.com>
2021-06-27 21:55:18 -0300

    [QuakeParser] (player) - Esta classe é responsável pela criação de jogadores. Armazena seus dados que são: nickname, contagem de kills e seu id.

---
+++ src/aplicacao/Player.java
@@ -0,0 +1,53 @@
+package aplicacao;
+
+public class Player {
+    
+    private String nickname;
+    private int personalKillCount;
+    private int id;
+    
+    public Player(String nickname, int id){
+        this.nickname = nickname;
+        this.personalKillCount = 0;
+        this.id = id;
+    }
+    
+    public Player(String nickname, int personalKillCount, int id){
+        this.nickname = nickname;
+        this.personalKillCount = personalKillCount;
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getPersonalKillCount() {
+        return personalKillCount;
+    }
+
+    public void increasedPersonalKillCount() {
+        personalKillCount = personalKillCount + 1;
+    }
+    
+    public void reducePersonalKillCount() {
+        personalKillCount = personalKillCount - 1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString(){
+        return nickname + ':' +personalKillCount;
+    }
+}
